9255bd8b20d3cf15e4aa2446718d3e56560c4987
itzmarkoni <benjamin.h.armendarez@outlook.com>
2025-06-13 21:05:59 -0400

    d

---
+++ main.lua
@@ -0,0 +1,22 @@
+local function run()
+    local monitor = peripheral.find("monitor")
+    local modem = peripheral.wrap("back")
+    monitor.setScale(1)
+    local maxWidth, maxHeight = monitor.getSize()
+    local y = 1
+    monitor.setCursorPos(1, y)
+    local init = "Initalizing stream...."
+    monitor.write(init)
+    print(init)
+    y = y+1
+    modem.open(80)
+
+    while true do
+        local _, _, _, _, message = os.pullEvent("modem_message")
+        print(message)
+        monitor.write(message)
+        y=y+1
+    end
+end
+
+return { run = run }
--- startup.lua
+++ startup.lua
@@ -1,18 +1,5 @@
-local monitor = peripheral.find("monitor")
-local modem = peripheral.wrap("back")
-monitor.setScale(1)
-local maxWidth, maxHeight = monitor.getSize()
-local y = 1
-monitor.setCursorPos(1, y)
-local init = "Initalizing stream...."
-monitor.write(init)
-print(init)
-y = y+1
-modem.open(80)
-
-while true do
-    local _, _, _, _, message = os.pullEvent("modem_message")
-    print(message)
-    monitor.write(message)
-    y=y+1
-end
+rm main.lua
+wget "https://raw.githubusercontent.com/itzmarkoni/NovaCrafLLC/refs/heads/main/main.lua"
+sleep(2)
+local main = require("main")
+main.run()
