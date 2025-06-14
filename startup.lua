1cfc79589afa974fe521e92801ee5031e5fce4d2
itzmarkoni <benjamin.h.armendarez@outlook.com>
2025-06-13 20:54:24 -0400

    updates

--- startup.lua
+++ startup.lua
@@ -1,1 +1,18 @@
-print("loaded from github")
+local monitor = peripheral.wrap("top")
+local modem = peripheral.wrap("back")
+monitor.setScale(1)
+local maxWidth, maxHeight = monitor.getSize()
+local y = 1
+monitor.setCursorPos(1, y)
+local init = "Initalizing stream...."
+monitor.write(init)
+print(init)
+y = y+1
+modem.open(80)
+
+while true do
+    local _, _, _, _, message = os.pullEvent("modem_message")
+    print(message)
+    monitor.write(message)
+    y=y+1
+end
